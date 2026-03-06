7f8ffbd80ca3d44abfc4a4c2d697e8ce63aa8625
Angry Coder <61619999+vincentvc168@users.noreply.github.com>
2026-03-06 22:28:08 +0800

    Create Getting_Started_with_Hugging_Faces.py

---
+++ Working_with_Hugging_Faces/Getting_Started_with_Hugging_Faces.py
@@ -0,0 +1,14 @@
+# Load the "validation" split of the TIGER-Lab/MMLU-Pro dataset
+my_dataset = load_dataset("TIGER-Lab/MMLU-Pro", split="validation")
+
+# Display dataset details
+print(my_dataset)
+
+---
+# Filter the documents
+filtered = wikipedia.filter(lambda row: "football" in row["text"])
+
+# Create a sample dataset
+example = filtered.select(range(1))
+
+print(example[0]["text"])
