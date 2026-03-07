021b5f0aff3bba28ef1d3df4443488870043074e
Angry Coder <61619999+vincentvc168@users.noreply.github.com>
2026-03-07 11:10:12 +0800

    Create AutoTokenizer.py

---
+++ Working_with_Hugging_Faces/AutoTokenizer.py
@@ -0,0 +1,10 @@
+# Download the model and tokenizer
+my_model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+my_tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+# Create the pipeline
+my_pipeline = pipeline(task="sentiment-analysis", model=my_model, tokenizer=my_tokenizer)
+
+# Predict the sentiment
+output = my_pipeline("This course is pretty good, I guess.")
+print(f"Sentiment using AutoClasses: {output[0]['label']}")
