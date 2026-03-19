041a1e30ff082aef14a5c9437bab229f8ac5ae53
Angry Coder <61619999+vincentvc168@users.noreply.github.com>
2026-03-19 16:00:36 +0800

    Create Structuring_End_to_End_Application.py

---
+++ Developing_AI_System_with_OpenAI_API/Structuring_End_to_End_Application.py
@@ -0,0 +1,15 @@
+# Create the OpenAI client
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Create the request
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+   {"role": "user", "content": "I have these notes with book titles and authors: New releases this week! The Beholders by Hester Musson, The Mystery Guest by Nita Prose. Please organize the titles and authors in a json file."}
+  ],
+  # Specify the response format
+  response_format={"type": "json_object"}
+)
+
+# Print the response
+print(response.choices[0].message.content)
